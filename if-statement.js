3a53fe6bc924f7459b9717b696d7eb93e5b4d6dd
Yosef Benny Widyokarsono <fybwid@gmail.com>
2018-09-10 23:19:23 +0800

    Update If statement

--- if-statement.js
+++ if-statement.js
@@ -1,7 +1,6 @@
 var fruit = 'orange';
 if (fruit.length > 5) {
   console.log('The fruit name has more than five characters.');
-}
-else {
+} else {
   console.log('The fruit name has five characters or less.');
 }
